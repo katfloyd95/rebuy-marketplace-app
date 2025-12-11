259912d6281e32ac2f590bdff108f6049be2c731
katfloyd95 <katarina.floyd1@gmail.com>
2025-12-11 19:09:22 +0100

    feat: implement header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 export default function Header() {
-    return <></>
+  return (
+    <HeaderContainer>
+      <HeaderLink to="/" role="homepage">
+        <h1>rebuy Marketplace</h1>
+      </HeaderLink>
+    </HeaderContainer>
+  );
 }
+
+const HeaderLink = styled(Link)`
+  text-decoration: none;
+  color: #f4f4f4;
+`;
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  padding: 16px;
+  box-sizing: border-box;
+  position: sticky;
+  top: 0;
+  z-index: 100;
+  background-color: #018790;
+`;
