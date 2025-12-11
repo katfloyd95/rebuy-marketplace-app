d1a9997a41d3240e39c87dca05cbce0dcb1b78e7
katfloyd95 <katarina.floyd1@gmail.com>
2025-12-11 18:39:28 +0100

    feat: create routing skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Details from './pages/Details';
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div>
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offer/:id" element={<Details />} />
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
